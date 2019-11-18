41d64d01b27e97c1d09b095e12552bb2fd0b9854
Cameron Bates <cameron@heyimcameron.com>
2019-11-18 12:57:37 -0500

    Use pushnotificationios

--- component/index.ios.js
+++ component/index.ios.js
@@ -1,7 +1,7 @@
 "use strict";
 
 import { AppState } from "react-native";
-import PushNotificationIOS from "@react-native-community/push-notification-ios";
+import { PushNotificationIOS } from "react-native";
 
 module.exports = {
   state: AppState,
